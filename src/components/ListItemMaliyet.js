90d24bde3dff8de89fe0701de73ebd7c1587aa43
kbratrnn <kbratrnn1907@gmail.com>
2018-04-21 19:14:27 +0300

    field cost was calculated

--- src/components/ListItemMaliyet.js
+++ src/components/ListItemMaliyet.js
@@ -2,39 +2,32 @@ import React, { Component } from 'react';
 import { Text, View, TouchableWithoutFeedback, Alert, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { Card, CardSection } from '../ortak';
-
-const sonuc = 0;
-
+let sonuc = 0.0;
 export default class ListItemMaliyet extends Component {
 
 hesapla(a, b, c) {
-
   sonuc = sonuc + a + b + c;
   return (a + b + c);
 }
 
   render() {
-const { tarlaAdi, urunAdi, urunCesidi, urunMaliyet, gubreCesitMaliyet, gubreTuruMaliyet, latitude , longitude } = this.props.tarla;
-
-    return (
-      <ScrollView>
-  <Card >
-        <CardSection>
-        <Text>
-          { tarlaAdi } - {urunAdi}- {urunCesidi}- {latitude}- {longitude}
-  </Text>
-  </CardSection>
-  <CardSection>
-            <Text>
-Maliyet:{this.hesapla(parseInt(urunMaliyet, 10), parseInt(gubreCesitMaliyet, 10), parseInt(gubreTuruMaliyet, 10)) }
-           TL
+const { tarlaAdi, urunAdi, urunCesidi, urunMaliyet, gubreCesitMaliyet, gubreTuruMaliyet } = this.props.tarla;
+      return (
+    <ScrollView>
+    <Card >
+    <CardSection>
+    <Text>
+    { tarlaAdi } - {urunAdi} - {urunCesidi}
     </Text>
-                    </CardSection>
-      </Card>
-
-          </ScrollView>
-
-
-    );
+    </CardSection>
+    <CardSection>
+    <Text>
+Maliyet:{this.hesapla(parseFloat(gubreCesitMaliyet, 10),
+  parseFloat(gubreTuruMaliyet, 10), parseFloat(urunMaliyet, 10)) }TL
+    </Text>
+    </CardSection>
+    </Card>
+    </ScrollView>
+    )
+    }
   }
-}
